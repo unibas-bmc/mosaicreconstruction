6ef7224645ba98addfd42813406d4bfe973e67db
Mattia Humbel <mattia.humbel@unibas.ch>
2022-11-08 22:50:12 +0100

    Setup trial reco.

--- MosaicReconstruction/trial_reco_cerebellum_8rings.py
+++ MosaicReconstruction/trial_reco_cerebellum_8rings.py
@@ -12,15 +12,15 @@ import sys
 
 import libtiff as tif
 
-###### settings, should all come from parameter file
-doBlock = 0          # 1: to block of 32 slices at once, else slice-wise reconstruction
+#### parameter to vary ####
+slice_no = 1024
+outputgrayscale = [0., 1.]
+
 method='gridrec'     # 'fbp' takes very long
 iterK = 2;           # number of iterations for iterative methods
-verboseExtra = 1;    # 1: more information
 
 ###### Input: Dataset to process
-#paramfile = '/media/griffin/_home1/mosaic_reconstruction/mousebrain/param_files/mouse4_perf_eth_full.txt'
-paramfile = sys.argv[1]
+paramfile = './example/param_files/Cerebellum_8Rings.txt'
 print('Using ' + paramfile)
 
 ###### 0.2 Read param file
@@ -34,7 +34,6 @@ with open(paramfile,'r') as f:
             name, valueAndMore = line.split("\t") # split it by tabs
             value, others = valueAndMore.split("\n")  # remove endline 
             infoStruct[name]=value 
-
 f.close()
 # assign variables
 samplename = infoStruct['samplename']
@@ -45,9 +44,6 @@ stripheight = float(infoStruct['stripheight'])
 outputcrop = infoStruct['outputcrop'].split(",")
 for i in range(len(outputcrop)):
     outputcrop[i] = int(outputcrop[i])
-outputgrayscale = infoStruct['outputgrayscale'].split(",")
-for i in range(len(outputgrayscale)):
-    outputgrayscale[i] = float(outputgrayscale[i])
 
 ###### 0.4 Set up directories
 projdir = projbasedir + samplename + os.sep + 'proj' + os.sep;
@@ -77,87 +73,66 @@ height = t.GetField("ImageLength")
 bits = t.GetField('BitsPerSample')
 sample_format = t.GetField('SampleFormat')
 typ = t.get_numpy_type(bits, sample_format)
-                
-###### 1.1 loop over y, load singogram, run reco, output reconstruction
-for b in range(nblocks):
-    print('Reconstructing block ' + str(b+1) + '/' + str(nblocks) + '...')
-    t1 = time.time()
-    tyi = b*blocksize+1;   # this y initial
-    tyf = (b+1)*blocksize; # this y final
-    tyr = range(tyi,tyf+1);  # this y range
-    
-    # load projection block 
-    projblock = np.empty((blocksize, sx, ip180), typ)
-    print('Loading projections...')
-    t2 = time.time()
-    for p in range(ip180):
-        t = tif.TIFF.open(projdir + 'proj_f_%04d' % (p+1) +  '.tif', 'r')
-        bits = t.GetField('BitsPerSample')
-        sample_format = t.GetField('SampleFormat')
-        typ = t.get_numpy_type(bits, sample_format)
-        tmp = np.empty((blocksize, width), typ)
-        size = tmp.nbytes
-        ReadStrip = t.ReadEncodedStrip
-        elem = ReadStrip(b, tmp.ctypes.data, size)
-        #tmp[np.isnan(tmp)] = 1;
-        projblock[:,:,p] = tmp;
-    
-    executionTime = (time.time() - t2)
-    print('read projections: %.2f sec ' % (executionTime))
 
-    # reshape into sinogram stack
-    projblock = np.transpose(projblock,(1,2,0));
-    sz=projblock.shape
-    
-    print('Reconstruction and writing...')
-    t3 = time.time()
-    if doBlock:
-        # do strip at once
-        this_sino_log = tomopy.minus_log(projblock)
-        this_sino_log = np.transpose(this_sino_log,(2,1,0))
-        if method=='fbp' or method=='gridrec':
-            reco = tomopy.recon(this_sino_log, angles, sinogram_order='False',algorithm=method)/pixsize_mm
-        else:
-            reco = tomopy.recon(this_sino_log, angles, sinogram_order='False',algorithm=method,num_iter=iterK)/pixsize_mm
+b = floor(slice_no / blocksize)
+print('Loading block ' + str(b+1) + '/' + str(nblocks) + '...')
+t1 = time.time()
+tyi = b*blocksize+1;   # this y initial
+tyf = (b+1)*blocksize; # this y final
+tyr = range(tyi,tyf+1);  # this y range
 
-        # crop
-        reco = reco[:,outputcrop[2]:sz[0]-outputcrop[3],outputcrop[0]:sz[0]-outputcrop[1]];
-        # convert to uint16
-        # reco = np.uint16(2**16*((reco-outputgrayscale[0])/(outputgrayscale[1]-outputgrayscale[0])));
-        reco = np.int16((2**16*((reco-outputgrayscale[0])/(outputgrayscale[1]-outputgrayscale[0])))-2**15);
-        
-        for iy in range(len(tyr)):
-            outfname = '%s/reco_%05d.tif' % ((recodir,tyr[iy]))
-            fid = tif.TIFF.open(outfname, 'w')
-            fid.write_image(np.squeeze(reco[iy,:,:]))
-            fid.close()
-    else:
-        for iy in range(len(tyr)):
-            this_sino_log = tomopy.minus_log(projblock[:,:,iy])
-            this_sino_log = np.transpose(this_sino_log)
-            this_sino_log = np.expand_dims(this_sino_log,axis=0)
-    
-            if method=='fbp' or method=='gridrec':
-                reco = tomopy.recon(this_sino_log, angles, sinogram_order='False',algorithm=method)/pixsize_mm
-            else:
-                reco = tomopy.recon(this_sino_log, angles, sinogram_order='False',algorithm=method,num_iter=iterK)/pixsize_mm
-            
-            reco = np.squeeze(reco)
-            # crop
-            reco = reco[outputcrop[2]:sz[0]-outputcrop[3],outputcrop[0]:sz[0]-outputcrop[1]];
-            # convert to int16
-            # reco = np.uint16(2**16*((reco-outputgrayscale[0])/(outputgrayscale[1]-outputgrayscale[0])));
-            reco = np.int16((2**16*((reco-outputgrayscale[0])/(outputgrayscale[1]-outputgrayscale[0])))-2**15);
+# load projection block 
+projblock = np.empty((blocksize, sx, ip180), typ)
+print('Loading projections...')
+t2 = time.time()
+for p in range(ip180):
+t = tif.TIFF.open(projdir + 'proj_f_%04d' % (p+1) +  '.tif', 'r')
+bits = t.GetField('BitsPerSample')
+sample_format = t.GetField('SampleFormat')
+typ = t.get_numpy_type(bits, sample_format)
+tmp = np.empty((blocksize, width), typ)
+size = tmp.nbytes
+ReadStrip = t.ReadEncodedStrip
+elem = ReadStrip(b, tmp.ctypes.data, size)
+#tmp[np.isnan(tmp)] = 1;
+projblock[:,:,p] = tmp;
 
-            outfname = '%s/reco_%05d.tif' % ((recodir,tyr[iy]))
-            fid = tif.TIFF.open(outfname, 'w')
-            fid.write_image(reco)
-            fid.close()
+executionTime = (time.time() - t2)
+print('read projections: %.2f sec ' % (executionTime))
 
-    executionTime = (time.time() - t3)
-    print('reconstruction: %.2f sec ' % (executionTime))
-    
-    executionTime = (time.time() - t1)
-    print('total time: %.2f sec ' % (executionTime))
+# reshape into sinogram stack
+projblock = np.transpose(projblock,(1,2,0));
+sz=projblock.shape
 
+print('Reconstruction and writing...')
+t3 = time.time()
+iy = slice_no % blocksize
+this_sino_log = tomopy.minus_log(projblock[:,:,iy])
+this_sino_log = np.transpose(this_sino_log)
+this_sino_log = np.expand_dims(this_sino_log,axis=0)
+
+if method=='fbp' or method=='gridrec':
+	reco = tomopy.recon(this_sino_log, angles,
+		sinogram_order='False',algorithm=method)/pixsize_mm
+else:
+	reco = tomopy.recon(this_sino_log, angles,
+		sinogram_order='False',algorithm=method,
+		num_iter=iterK)/pixsize_mm
     
+reco = np.squeeze(reco)
+# crop
+reco = reco[outputcrop[2]:sz[0]-outputcrop[3],outputcrop[0]:sz[0]-outputcrop[1]];
+# convert to int16
+# reco = np.uint16(2**16*((reco-outputgrayscale[0])/(outputgrayscale[1]-outputgrayscale[0])));
+reco = np.int16((2**16*((reco-outputgrayscale[0])/(outputgrayscale[1]-outputgrayscale[0])))-2**15);
+
+outfname = '%s/reco_%05d.tif' % ((recodir,tyr[iy]))
+fid = tif.TIFF.open(outfname, 'w')
+fid.write_image(reco)
+fid.close()
+
+executionTime = (time.time() - t3)
+print('reconstruction: %.2f sec ' % (executionTime))
+
+executionTime = (time.time() - t1)
+print('total time: %.2f sec ' % (executionTime))
