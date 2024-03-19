7fbd997afee14845b35d91347786b73b293ad4d4
Mattia Humbel <mattia.humbel@unibas.ch>
2024-03-19 17:56:37 +0100

    Change ReadAngles to take abs of angles.

--- MosaicReconstruction/utils/metadata/ReadAngles_ParamFile.m
+++ MosaicReconstruction/utils/metadata/ReadAngles_ParamFile.m
@@ -28,12 +28,8 @@ nxsfile = [rawdatapath scandir scansuffix filesep scandir scansuffix '.nxs'];
 find_index = @(v,val) find(abs(v-val) == min(abs(v-val)),1);
 
 angles = h5read(nxsfile,h5AnglePath);
-angles = angles - angles(1);
-if angles(2) > 0
-    ip360 = find_index(angles,360);% index of projection with angle 360 degrees
-else
-    ip360 = find_index(angles,-360);% index of projection with angle -360 degrees
-end
+angles = abs(angles - angles(1));
+ip360 = find_index(angles,360);% index of projection with angle 360 degrees
 ip180 = floor(ip360/2); % index of projection with angle 180 degrees
 angles = angles(1:ip180);
 
